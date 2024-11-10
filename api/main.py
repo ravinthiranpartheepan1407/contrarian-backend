48cf9860e2da74e1edbf8f2573cadd0694a7146c
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2024-11-10 14:51:44 +0200

    API fix

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import List, Optional
 from dotenv import load_dotenv
 import PyPDF2
 import io
